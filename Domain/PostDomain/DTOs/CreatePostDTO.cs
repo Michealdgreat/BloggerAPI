36cd103f09b7c00041f16dc17c71f50157865482
Micheal Shodamola <michealshodamola01@gmail.com>
2024-04-28 02:33:59 +0300

    Added New Data to Object for Post creation

---
+++ Domain/PostDomain/DTOs/CreatePostDTO.cs
@@ -0,0 +1,22 @@
+﻿using Domain.CategoryDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.PostDomain.DTOs
+{
+    public class CreatePostDTO
+    {
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public string PostUrl { get; set; }
+        public required string Content { get; set; }
+        public required string FeaturedImageUrl { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public required string Author { get; set; }
+        public bool IsVisible { get; set; }
+        public required List<Category> Categories { get; set; }
+    }
+}
