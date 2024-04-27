cf8932a763db7ca385992de621e2ccfd2a672eaf
Micheal Shodamola <michealshodamola01@gmail.com>
2024-04-28 00:34:00 +0300

    Added Delete Handler

---
+++ Application/Post/DeletePost/DeletePostHandler.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.DataAccess;
+using MediatR;
+
+
+namespace Application.Post.DeletePost;
+
+public class DeletePostHandler(IRepositoryBase repositoryBase) : IRequest<DeletePostCommand>
+{
+    private readonly IRepositoryBase _repoBase = repositoryBase;
+
+    public Task Handle(DeletePostCommand request)
+    {
+        _repoBase.ToDataBase<dynamic>("", new { }, "");
+
+        return Task.CompletedTask;
+    }
+
+}
