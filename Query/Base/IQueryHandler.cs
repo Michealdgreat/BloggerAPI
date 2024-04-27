f680c11f64fea34c3cae3a824f10748a7f08bd3a
Micheal Shodamola <michealshodamola01@gmail.com>
2024-04-28 02:35:16 +0300

    Query handler

---
+++ Query/Base/IQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Query.Base
+{
+    public interface IQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse> where TResponse : class? //TQuery is the request, TResponse is the return type.
+    {
+    }
+}
