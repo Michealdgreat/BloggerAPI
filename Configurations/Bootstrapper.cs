71a5a2101c705a92e1d310039fbdadda42e215a9
Micheal Shodamola <michealshodamola01@gmail.com>
2024-04-28 18:42:57 +0300

    Added Major DI Class

---
+++ Configurations/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Application.Post.CreatePost;
+using MediatR;
+using Infrastructure;
+using PresentationFacade;
+using System.Reflection;
+using System;
+
+
+namespace Configurations;
+
+public static class Bootstrapper
+{
+    public static void ConfigBootstrapper(this IServiceCollection services)
+    {
+        InfrastructureBootstrapper.InfrastructureDependency(services);
+        FacadeBootstrapper.FacadeLibraryDependency(services);
+
+        services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+    }
+}
