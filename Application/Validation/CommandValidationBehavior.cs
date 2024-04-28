c3bff90c2a653ed0881f902f04789c87c2c69374
Micheal Shodamola <michealshodamola01@gmail.com>
2024-04-28 18:41:54 +0300

    Validation Utility class

---
+++ Application/Validation/CommandValidationBehavior.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validation;
+
+public class CommandValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IBaseRequest
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public CommandValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var errors = _validators
+            .Select(v => v.Validate(request))
+            .SelectMany(result => result.Errors)
+            .Where(error => error != null)
+            .ToList();
+
+        if (errors.Count != 0)
+        {
+            var errorBuilder = new StringBuilder();
+
+            foreach (var error in errors)
+            {
+                errorBuilder.AppendLine(error.ErrorMessage);
+            }
+
+            throw new InvalidCommandException(errorBuilder.ToString());
+        }
+        var response = await next();
+        return response;
+    }
+}
