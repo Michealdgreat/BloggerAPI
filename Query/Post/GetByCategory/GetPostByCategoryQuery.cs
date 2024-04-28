1d9a1cbf9d091daaf92fc27d4040ddc0d5fd0fcb
Micheal Shodamola <michealshodamola01@gmail.com>
2024-04-28 15:04:09 +0300

    Added Get Post by category Query and Handler

---
+++ Query/Post/GetByCategory/GetPostByCategoryQuery.cs
@@ -0,0 +1,12 @@
+﻿using Domain.PostDomain;
+using Query.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Query.Post.GetByCategory
+{
+    public record GetPostByCategoryQuery(string value) : IQuery<List<PostDTO>>;
+}
---
+++ Query/Post/GetByCategory/GetPostByCategoryQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Domain.PostDomain;
+using Infrastructure.DataAccess;
+using Query.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Query.Post.GetByCategory;
+
+public class GetPostByCategoryQueryHandler(IRepositoryBase repositoryBase) : IQueryHandler<GetPostByCategoryQuery, List<PostDTO>>
+{
+
+    private readonly IRepositoryBase _repoBase = repositoryBase;
+
+    public async Task<List<PostDTO>> Handle(GetPostByCategoryQuery request, CancellationToken cancellationToken)
+    {
+        await _repoBase.FromDataBase<PostDTO, dynamic>("dbo.GetPostsByCategory", new { request.value }, "BloggerConnStrings");
+
+        return await Task.FromResult(new List<PostDTO>());
+    }
+}
+
