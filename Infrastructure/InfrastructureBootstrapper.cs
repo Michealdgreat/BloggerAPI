8fc0f0ca4170d309c7e7da5d5991e350f2056c22
Micheal Shodamola <michealshodamola01@gmail.com>
2024-04-28 18:43:39 +0300

    Infrastructure Library Dependency Injection

---
+++ Infrastructure/InfrastructureBootstrapper.cs
@@ -0,0 +1,12 @@
+﻿using Infrastructure.DataAccess;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class InfrastructureBootstrapper
+{
+    public static void InfrastructureDependency(this IServiceCollection services)
+    {
+        services.AddTransient<IRepositoryBase, RepositoryBase>();
+    }
+}
