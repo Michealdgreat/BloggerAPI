7820e00c2719c8f821b8e958bd77ece61c18e3ea
Micheal Shodamola <michealshodamola01@gmail.com>
2024-04-29 02:54:38 +0300

    Addedd CommandHandler DI to Startup class

--- BloggerAPI/Program.cs
+++ BloggerAPI/Program.cs
@@ -1,24 +1,34 @@
 using Microsoft.AspNetCore.Hosting;
 using System.Reflection;
 using BlogConfig;
+using Application;
+using Microsoft.Extensions.DependencyInjection;
+using Application.Post.CreatePost;
 
 namespace BloggerAPI
 {
     public class Program
     {
-        private static readonly IServiceCollection services;
 
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var services = builder.Services;
+            services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 
             // Add services to the container.
-            Bootstrapper.ConfigBootstrapper(services);
+
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            services.AddMediatR(cf => cf.RegisterServicesFromAssembly(typeof(CreatePostCommandHandler).Assembly));
+
+
+            Bootstrapper.ConfigBootstrapper(services);
+
 
             var app = builder.Build();
 
