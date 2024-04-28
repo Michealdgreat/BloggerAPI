ff58fa8a6f500636bef13c224da5888390c7629b
Micheal Shodamola <michealshodamola01@gmail.com>
2024-04-28 03:20:07 +0300

    Get All Posts List Query and Handler

---
+++ Query/Post/GetAll/GetPostListQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Domain.PostDomain;
+using Infrastructure.DataAccess;
+using Query.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Query.Post.GetAll
+{
+    public class GetPostListQueryHandler(IRepositoryBase repositoryBase) : IQueryHandler<GetPostListQuery, List<PostDTO>>
+    {
+        private readonly IRepositoryBase _repoBase = repositoryBase;
+
+        public async Task<List<PostDTO>> Handle(GetPostListQuery request, CancellationToken cancellationToken)
+        {
+
+            var result = await _repoBase.FromDataBase<PostDTO, dynamic>("GetAllPosts", new { }, "BloggerConnStrings");
+
+            return result;
+            //return Task.FromResult(new List<PostDTO>());
+        }
+    }
+}
