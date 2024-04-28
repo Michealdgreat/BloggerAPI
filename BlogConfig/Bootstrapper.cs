e6e93cf1937101a80ba47c47fcef9e8fbe2aa2ab
Micheal Shodamola <michealshodamola01@gmail.com>
2024-04-29 02:52:59 +0300

    Fixed Dependency Injection assembly reference

--- BlogConfig/Bootstrapper.cs
+++ BlogConfig/Bootstrapper.cs
@@ -12,7 +12,10 @@ public static class Bootstrapper
     public static void ConfigBootstrapper(this IServiceCollection services)
     {
         InfrastructureBootstrapper.InfrastructureDependency(services);
-        services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        // services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        services.AddMediatR(cf => cf.RegisterServicesFromAssembly(typeof(Bootstrapper).Assembly));
+
         FacadeBootstrapper.FacadeLibraryDependency(services);
 
     }
