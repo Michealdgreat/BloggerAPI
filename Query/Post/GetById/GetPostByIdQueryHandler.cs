a909f45f637ca6122d69a85913cf6291b77a03c5
Micheal Shodamola <michealshodamola01@gmail.com>
2024-04-28 03:20:47 +0300

    Get Post By Id Query and Handler

---
+++ Query/Post/GetById/GetPostByIdQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using Domain.PostDomain;
+using Infrastructure.DataAccess;
+using Query.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Query.Post.GetById
+{
+    public class GetPostByIdQueryHandler(IRepositoryBase repositoryBase) : IQueryHandler<GetPostByIdQuery, PostDTO>
+    {
+        private readonly IRepositoryBase _repoBase = repositoryBase;
+
+        public async Task<PostDTO> Handle(GetPostByIdQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _repoBase.GetOneFromDataBase<PostDTO, dynamic>("dbo.GetPost", new { @PostId = request.id }, "BloggerConnStrings");
+
+            return result;
+        }
+    }
+}
